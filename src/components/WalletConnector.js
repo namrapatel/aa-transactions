82937aa24a4943e7d7b2a5aedecc79b699bd3bc8
Namra Patel <namrajpatel@gmail.com>
2021-08-25 10:29:03 -0600

    userInfo working

--- src/components/WalletConnector.js
+++ src/components/WalletConnector.js
@@ -1,10 +1,32 @@
-import React from 'react';
-import { ethers } from "ethers";
+import React, {useEffect, useState} from 'react';
+import { ethers, utils } from "ethers";
+import { formatEther } from 'ethers/lib/utils';
 
 function WalletConnector(props) {
+    const [userAddress, setUserAddress] = useState("");
+    const [userBalance, setUserBalance] = useState(0);
+
     let provider;
     window.ethereum.enable().then(provider = new ethers.providers.Web3Provider(window.ethereum));
     const signer = provider.getSigner();
+
+    useEffect(() => {
+        async function getUserAddress() {
+            await signer.getAddress().then(result => {
+                setUserAddress(result);
+            })
+        }
+        getUserAddress()
+
+        async function getUserBalance() {
+            await signer.getBalance().then(result => {
+                var ether = formatEther(result);
+                var num = parseFloat(ether, 10).toFixed(4);
+                setUserBalance(num);
+            })
+        }
+        getUserBalance()
+      }, []);
 
     return(
     <div className="WalletConnector">
@@ -12,11 +34,11 @@ function WalletConnector(props) {
             <div className="button">
                 Connect wallet
             </div> :
-            <div>
-                {signer.getAddress()}
+            <div className="userInfo">
+                <div className="userBalance">{userBalance + " ETH"}</div>
+                <div className="userAddress">{userAddress}</div>
             </div>
         }
-       
     </div>)
     
 }
